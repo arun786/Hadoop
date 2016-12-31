a5a49b208918118f29a45d61b3b5d9f341989dae
arun789 <adwiti1975@gmail.com>
2016-12-31 01:19:59 -0700

    scripts

--- MaximumTemperature/src/main/resources/script.sh
+++ MaximumTemperature/src/main/resources/script.sh
@@ -1,2 +1,3 @@
-
+#!/bin/bash
+hadoop fs -rm -r /Arun/Mapreduce/MaxTemperature/FilesToWrite/*
 hadoop jar MaximumTemperature.jar com.arun.job.MaximumTemperatureDiver /Arun/Mapreduce/MaxTemperature/FilesToRead /Arun/Mapreduce/MaxTemperature/FilesToWrite/output
